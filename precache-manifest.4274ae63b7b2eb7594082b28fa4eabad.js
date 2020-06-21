4caf202a56590a0b7ea5314ec180a4aea0175405
johndmguerrero <guerrero.johndm@gmail.com>
2020-06-22 00:42:59 +0800

    Updates

---
+++ precache-manifest.4274ae63b7b2eb7594082b28fa4eabad.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ec995ceb026825b87e2284e25f734d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18b813aee351b4ea670b",
+    "url": "/static/css/main.e29eec4e.chunk.css"
+  },
+  {
+    "revision": "578ec4f6cc16bb86e0b6",
+    "url": "/static/js/2.82436bad.chunk.js"
+  },
+  {
+    "revision": "8e1f2a45f25560b4c98213b97585f69d",
+    "url": "/static/js/2.82436bad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b813aee351b4ea670b",
+    "url": "/static/js/main.6e0e1af4.chunk.js"
+  },
+  {
+    "revision": "9c6eef695b7f5c695322",
+    "url": "/static/js/runtime-main.2e32fe4e.js"
+  },
+  {
+    "revision": "e34d25273925d9c9e8ea0b9de7aece00",
+    "url": "/static/media/Github.e34d2527.svg"
+  },
+  {
+    "revision": "d377e7868d947fb8d9741d2b029d02d5",
+    "url": "/static/media/Linkedn.d377e786.svg"
+  },
+  {
+    "revision": "226b4c7c5ba7140afd0857ada083d5e7",
+    "url": "/static/media/Logo.226b4c7c.svg"
+  },
+  {
+    "revision": "0af8f267d7ab9e6631ecda3fd2d09a0f",
+    "url": "/static/media/TTCommons-Bold.0af8f267.woff"
+  },
+  {
+    "revision": "1331b82cd5ba83ec41d8d4068affd96e",
+    "url": "/static/media/TTCommons-ExtraBold.1331b82c.woff"
+  },
+  {
+    "revision": "41007c1f9c6f7654d4b6e029e31996f2",
+    "url": "/static/media/TTCommons-Medium.41007c1f.woff"
+  },
+  {
+    "revision": "4902456230df016321b7da92b8144e29",
+    "url": "/static/media/TTCommons-Regular.49024562.woff"
+  },
+  {
+    "revision": "5e10f16ea4e584726c859a012541a220",
+    "url": "/static/media/juan-alert.5e10f16e.jpg"
+  },
+  {
+    "revision": "c9d4756e1b1fa42539a1dee591fdc4ba",
+    "url": "/static/media/nppl.c9d4756e.jpg"
+  },
+  {
+    "revision": "4336af55f8bbd3bb374e8212a8f2160f",
+    "url": "/static/media/perpetual-help.4336af55.jpg"
+  },
+  {
+    "revision": "4884d63b01d88bfb5c1035229906975a",
+    "url": "/static/media/tomoe.4884d63b.jpg"
+  },
+  {
+    "revision": "5162f1b6727c89c9fb8a41a75ccb8dc3",
+    "url": "/static/media/womo.5162f1b6.jpg"
+  }
+]);
